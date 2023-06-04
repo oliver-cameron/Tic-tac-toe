3b3f23a3fd4518d1160515cbe23b7688768b4e35
oliver-cameron <94218640+oliver-cameron@users.noreply.github.com>
2023-06-04 11:41:24 +1000

    FIrst working Prototype

---
+++ tic-tac-toe.js
@@ -0,0 +1,11 @@
+var turn = true;
+
+function a1(){if(turn){document.getElementById("a1X").style.display = "block"}else{document.getElementById("a1O").style.display = "block"} turn = !turn}
+function a2(){if(turn){document.getElementById("a2X").style.display = "block"}else{document.getElementById("a2O").style.display = "block"} turn = !turn}
+function a3(){if(turn){document.getElementById("a3X").style.display = "block"}else{document.getElementById("a3O").style.display = "block"} turn = !turn}
+function b1(){if(turn){document.getElementById("b1X").style.display = "block"}else{document.getElementById("b1O").style.display = "block"} turn = !turn}
+function b2(){if(turn){document.getElementById("b2X").style.display = "block"}else{document.getElementById("b2O").style.display = "block"} turn = !turn}
+function b3(){if(turn){document.getElementById("b3X").style.display = "block"}else{document.getElementById("b3O").style.display = "block"} turn = !turn}
+function c1(){if(turn){document.getElementById("c1X").style.display = "block"}else{document.getElementById("c1O").style.display = "block"} turn = !turn}
+function c2(){if(turn){document.getElementById("c2X").style.display = "block"}else{document.getElementById("c2O").style.display = "block"} turn = !turn}
+function c3(){if(turn){document.getElementById("c3X").style.display = "block"}else{document.getElementById("c3O").style.display = "block"} turn = !turn}
